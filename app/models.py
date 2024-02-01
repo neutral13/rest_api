a35dee53e9b0555fd950354600a6d3ed4909b29d
neutral <neutral432@gmail.com>
2024-02-01 17:00:26 +0600

    first commit

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), index=True)
+    email = Column(String(50), unique=True, index=True)
+    password = Column(String(50))
